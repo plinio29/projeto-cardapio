67d51de4c27a90197014ace90aeb22af6ae9e2ae
Plínio Ramos <plinio206@live.com>
2024-01-29 02:42:01 -0300

    Ajustes gerais no HTML, CSS e JS.

--- script/funcaohora.js
+++ script/funcaohora.js
@@ -2,14 +2,16 @@
 var funcao1 = document.getElementById('funcao');
 var funcao2 = document.getElementById('funcao2');
 
-var numero = 2;
+var data = new Date()
+var hora = data.getHours()
 
-if (numero === 2){
+
+if (hora > 1 && hora < 17){
     funcao1.style.display = "none";
     funcao2.style.display = "block";
-} else if (numero = 1){
+} else if (hora > 17 && hora < 24){
     funcao2.style.display = "none";
     funcao1.style.display = "block";
 } else {
-    console.log("vai curinthia");
+    console.log("Erro!");
 }
