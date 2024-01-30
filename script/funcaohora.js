bde0cad5804ee94f6e1e3e3e6fe0c47762958b3d
Plínio Ramos <plinio206@live.com>
2024-01-30 02:47:15 -0300

    Ajustes no JS e CSS.

--- script/funcaohora.js
+++ script/funcaohora.js
@@ -7,10 +7,8 @@ var hora = data.getHours()
 
 
 if (hora > 1 && hora < 17){
-    funcao1.style.display = "none";
     funcao2.style.display = "block";
 } else if (hora > 17 && hora < 24){
-    funcao2.style.display = "none";
     funcao1.style.display = "block";
 } else {
     console.log("Erro!");
