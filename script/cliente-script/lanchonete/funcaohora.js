fe3ed0054018fe18b48e2c7683914be5e6829a2c
Plínio Ramos <plinio206@live.com>
2024-03-17 17:16:00 -0300

    Update funcaohora.js

--- script/cliente-script/lanchonete/funcaohora.js
+++ script/cliente-script/lanchonete/funcaohora.js
@@ -7,13 +7,11 @@ var hora = data.getHours()
 var dia = data.getDay()
 
 
-if (hora > 0 && hora < 17 && dia || 0){
+if (hora > 0 && hora < 17){
     funcao2.style.display = "block";
 
-}else if (hora > 0 && dia == 0){
-        funcao2.style.display = "block"; 
 }else{
-    funcao1.style.display = "block";
+    funcao1.style.display = "block"; 
     console.log("Hoje é dia", dia);
     console.log("A hora é", hora);
 }
